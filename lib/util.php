2104cfa987d41ae17fa3ccc68981c27a262e6d96
Duncan Chiang <duncan.c@inwinstack.com>
2018-03-05 11:51:16 +0800

    [Feature] Add firstLogin information in preferences table.

--- lib/util.php
+++ lib/util.php
@@ -57,7 +57,8 @@ class Util {
         }
         
         self::saveOpenIDInfoToDB($userInfo,$isOpenIDUser=true);
-        
+        $config = \OC::$server->getConfig();
+        $config->setUserValue($userID, "login", "firstLogin", time());
         self::wirteAuthInfoToSession($authInfo);
         return \OC_User::login($userID, $password);
     }
